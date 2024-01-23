24f82f3e750604276715dba65c8ce5f7b44732d5
Maxime Lacasse <mlacassegermain@cmaisonneuve.qc.ca>
2024-01-23 17:03:28 -0500

    Navigation complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("test");
+(function () {
+    const nav = document.querySelector("nav");
+
+    nav.addEventListener("click", function () {
+        const estOuvert = nav.dataset.opened === "true";
+        nav.dataset.opened = !estOuvert;
+    });
+})();
