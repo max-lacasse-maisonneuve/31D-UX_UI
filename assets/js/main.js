886cde28a6449d8a58806021b525b036cd1d8938
Maxime Lacasse <mlacassegermain@cmaisonneuve.qc.ca>
2024-01-24 10:41:34 -0500

    Ajout d'interactivite navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("test");
+// Récuperer la navigation
+const nav = document.querySelector("nav");
+
+// Ajouter un écouteur d'événement sur le clic
+nav.addEventListener("click", function () {
+    const isOpen = nav.dataset.opened === "true";
+    nav.dataset.opened = !isOpen;
+});
